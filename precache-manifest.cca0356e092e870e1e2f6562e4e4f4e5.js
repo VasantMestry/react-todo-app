5045f8ab696c296700b472961e372b252256b941
VasantMestry <vasant.mestry@deuexsolutions.com>
2020-10-13 12:49:33 +0530

    Updates

---
+++ precache-manifest.cca0356e092e870e1e2f6562e4e4f4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef94ac8ffe8b6107d6514feab4c325d",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "e071ec9c859efee8c71d",
+    "url": "/react-todo-app/static/css/main.fe0d2a33.chunk.css"
+  },
+  {
+    "revision": "ebc5b58ca9e4876b4f0b",
+    "url": "/react-todo-app/static/js/2.2d11cd87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.2d11cd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e071ec9c859efee8c71d",
+    "url": "/react-todo-app/static/js/main.82890705.chunk.js"
+  },
+  {
+    "revision": "77ef626dbdd4b1e53296",
+    "url": "/react-todo-app/static/js/runtime-main.3db00947.js"
+  }
+]);
